752eb2ade68836abff4e3222e963a97699433ae6
Dungx2 <dvdungg06@gmail.com>
2025-04-07 22:29:12 +0700

    dung truoc con bao

---
+++ src/app/data/homeData.ts
@@ -0,0 +1,78 @@
+import { TextAndImageProps } from "@/components/home/TextAndImage/TextAndImage";
+
+export const heroData = {
+    heading: "Elevate Your Senses with Timeless Scents",
+    body: "At Raumania Fragrance, we craft luxurious scents that capture emotions, memories, and moments. Each fragrance is designed to elevate your senses, blending the finest ingredients to create timeless aromas.",
+    buttonText: "Custom Your Perfumes",
+    buttonHref: "/explore",
+    color: "yellow",
+  };
+  
+  export const productsData = [
+    {
+      id: "perfume-1",
+      name: "Velvet Bloom",
+      imageUrl: "/images/fragrance/VelvetBloom.png",
+      price: 50000000,
+      customizeUrl: "/customize/street-rocket",
+    },
+    {
+        id: "perfume-1",
+        name: "Velvet Bloom",
+        imageUrl: "/images/fragrance/VelvetBloom.png",
+        price: 50000000,
+        customizeUrl: "/customize/street-rocket",
+      },
+      {
+        id: "perfume-1",
+        name: "Velvet Bloom",
+        imageUrl: "/images/fragrance/VelvetBloom.png",
+        price: 50000000,
+        customizeUrl: "/customize/street-rocket",
+      },
+      {
+        id: "perfume-1",
+        name: "Velvet Bloom",
+        imageUrl: "/images/fragrance/VelvetBloom.png",
+        price: 50000000,
+        customizeUrl: "/customize/street-rocket",
+      },
+    
+    // Bạn có thể thêm các sản phẩm khác tại đây
+  ];
+  
+  export const textAndImageSlices: TextAndImageProps[] = [
+    {
+      index: 1,
+      theme: "be",
+      heading: "I want to be part of what’s coming next",
+      body: "At Raumania, our sustainability efforts are rooted in the belief that we must focus where we can make the most meaningful difference. We are committed to restoring nature, protecting the climate, investing in circularity, and championing dignity and opportunity for all—especially by advancing women’s autonomy.",
+      buttonText: "Shop Now",
+      buttonHref: "/products",
+      variation: "imageOnLeft",
+      foregroundImage: "/images/guy-1.png",
+      backgroundImage: "/images/paint-background.png",
+    },
+    {
+      index: 2,
+      theme: "milk",
+      heading: "I want to be part of what’s coming next",
+      body: "At Raumania, our sustainability efforts are rooted in the belief that we must focus where we can make the most meaningful difference. We are committed to restoring nature, protecting the climate, investing in circularity, and championing dignity and opportunity for all—especially by advancing women’s autonomy.",
+      buttonText: "Shop Now",
+      buttonHref: "/products",
+      variation: "default",
+      foregroundImage: "/images/guy-1.png",
+      backgroundImage: "/images/paint-background.png",
+    },
+    {
+      index: 3,
+      theme: "be",
+      heading: "Empowering Women’s Autonomy",
+      body: "Raumania was founded by Nhungnguoiae36, a visionary who redefined women’s fashion and took control of her own destiny. Women have always been at the heart of everything we do. Ensuring that women have an equal voice and the freedom to safely choose their own path is a timeless part of our legacy. Today, we continue to support women—both within and beyond our House—in their pursuit of autonomy, with the aim of amplifying the impact of gender equality around the world.",
+      buttonText: "Shop Now",
+      buttonHref: "/products",
+      variation: "imageOnLeft",
+      foregroundImage: "/images/guy-1.png",
+      backgroundImage: "/images/paint-background.png",
+    },
+  ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { ButtonLink } from "@/components/ButtonLink";
+
 import { Header } from "@/components/Header";
 import Hero from "@/components/home/Hero";
 import ProductGrid from "@/components/home/ProductGrid/ProductGrid";
 import TextAndImage from "@/components/home/TextAndImage/TextAndImage";
-import Image from 'next/image';
+
+import { heroData, productsData, textAndImageSlices } from "./data/homeData";
+import VideoBlock from "@/components/home/VideoBlock/videoblock";
 
 
 
@@ -13,89 +15,29 @@ export default function Home() {
     <>
      <Header/>
      <Hero
-      heading="Elevate Your Senses with Timeless Scents"
-      body="At Raumania Fragrance, we craft luxurious scents that capture emotions, memories, and moments. Each fragrance is designed to elevate your senses, blending the finest ingredients to create timeless aromas."
-      buttonText="Custom Your Perfumes"
-      buttonHref="/explore"
-      color="yellow"
-    />
-     <ProductGrid
-      heading="Our Perfumes"
-      body="Browse our luxurious collection of signature scents."
-      products={[
-        {
-          id: "perfumes-1",
-          name: "Velvet Bloom",
-          imageUrl: "/images/fragrance/VelvetBloom.png",
-          price: 50000000,
-          customizeUrl: "/customize/street-rocket",
-        },
-        {
-          id: "perfumes-1",
-          name: "Velvet Bloom",
-          imageUrl: "/images/fragrance/VelvetBloom.png",
-          price: 50000000,
-          customizeUrl: "/customize/street-rocket",
-        },
-        {
-          id: "perfumes-1",
-          name: "Velvet Bloom",
-          imageUrl: "/images/fragrance/VelvetBloom.png",
-          price: 50000000,
-          customizeUrl: "/customize/street-rocket",
-        },
-        {
-          id: "perfumes-1",
-          name: "Velvet Bloom",
-          imageUrl: "/images/fragrance/VelvetBloom.png",
-          price: 50000000,
-          customizeUrl: "/customize/street-rocket",
-        },
-        
-      ]}
+        heading={heroData.heading}
+        body={heroData.body}
+        buttonText={heroData.buttonText}
+        buttonHref={heroData.buttonHref}
+        color={heroData.color}
+      />
+
+      <ProductGrid
+        heading="Our Perfumes"
+        body="Browse our luxurious collection of signature scents."
+        products={productsData}
+      />
+
+      {textAndImageSlices.map((slice, index) => (
+        <TextAndImage key={index} {...slice} />
+      ))}
     
-    />
-    <TextAndImage
-        index={1}
-        theme="be"
-        heading="I want to be part of what’s coming next"
-        body="At Raumania, our sustainability efforts are rooted in the belief that we must focus where we can make the most meaningful difference. We are committed to restoring nature, protecting the climate, investing in circularity, and championing dignity and opportunity for all—especially by advancing women’s autonomy.
-In the face of today’s urgent challenges and those ahead, our goal is to be part of what endures: a future where people, nature, and business thrive together."
-        buttonText="Shop Now"
-        buttonHref="/products"
-        variation="imageOnLeft"
-        foregroundImage="/images/guy-1.png"
-        backgroundImage="/images/paint-background.png"
-        
-    />
-    <TextAndImage
-        index={2}
-        theme="milk"
-        heading="I want to be part of what’s coming next"
-        body="At Raumania, our sustainability efforts are rooted in the belief that we must focus where we can make the most meaningful difference. We are committed to restoring nature, protecting the climate, investing in circularity, and championing dignity and opportunity for all—especially by advancing women’s autonomy.
-In the face of today’s urgent challenges and those ahead, our goal is to be part of what endures: a future where people, nature, and business thrive together."
-        buttonText="Shop Now"
-        buttonHref="/products"
-        variation="default"
-        foregroundImage="/images/guy-1.png"
-        backgroundImage="/images/paint-background.png"
-        
-    />
-    <TextAndImage
-        index={3}
-        theme="be"
-        heading="Empowering Women’s Autonomy"
-        body="Raumania was founded by Nhungnguoiae36, a visionary who redefined women’s fashion and took control of her own destiny.
-Women have always been at the heart of everything we do. Ensuring that women have an equal voice and the freedom to safely choose their own path is a timeless part of our legacy. Today, we continue to support women—both within and beyond our House—in their pursuit of autonomy, with the aim of amplifying the impact of gender equality around the world.
-"
-        buttonText="Shop Now"
-        buttonHref="/products"
-        variation="imageOnLeft"
-        foregroundImage="/images/guy-1.png"
-        backgroundImage="/images/paint-background.png"
-        
-    />
-    
+    <VideoBlock youtubeID="VllN0yINA5A" />
+    <ProductGrid
+        heading="Our Perfumes"
+        body="Browse our luxurious collection of signature scents."
+        products={productsData}
+      />
     
     </>
   );
